ce7d61ceddb0aa7e12fbeb0f5cb8ef76f37a6305
“LoicFabre” <loic.fabre@etudiant.univ-lille1.fr>
2016-01-29 11:54:16 +0100

    Logique tp2

--- Logique/tp2/tp2.pl
+++ Logique/tp2/tp2.pl
@@ -28,3 +28,51 @@ extraire_tete(XS,N,L) :- ajoute_en_tete(N,L,XS).
 concatene([],YS,YS).
 concatene([X|XS],YS,[X|L]) :- concatene(XS,YS,L).
 
+% Q8
+retourne([],[],[]).
+retourne([X],YS,[X|YS]).
+retourne([X|XS],YS,A) :- retourne(XS,[X|YS],A).
+
+% Q9
+insert_trie(N,[],[N]).
+insert_trie(N,[X|XS],[N|[X|XS]]) :- N<X, !.
+insert_trie(N,[X|XS],[X|L]) :- insert_trie(N,XS,L).
+
+% Q10
+tri_insert([X],[X]).
+tri_insert([X|XS],L):- tri_insert(XS,L2), insert_trie(X,L2,L).
+
+% Q11
+divise([],[],[]).
+divise([X],[X],[]).
+divise([X|[Y|XS]],[X|L],[Y|L2]) :- divise(XS,L,L2).
+
+% Q12
+fusion(XS,[],XS).
+fusion([],YS,YS).
+fusion([X|XS],[Y|YS],[X|L]) :- X<Y, fusion(XS,[Y|YS],L).
+fusion([X|XS],[Y|YS],[Y|L]) :- fusion([X|XS],YS,L).
+
+% Q13
+tri_fusion([X],[X]).
+tri_fusion(ZS,L):- divise(ZS,XS,YS), tri_fusion(XS,P), tri_fusion(YS,P2), fusion(P,P2,L), !.
+
+% Q14
+balance(_,[],[],[]):- !.
+balance(X,[Y|YS],[Y|L1],L2) :- Y<X, balance(X,YS,L1,L2),!.
+balance(X,[Y|YS],L1,[Y|L2]) :- balance(X,YS,L1,L2),!.
+
+% Q15
+tri_rapide([],[]).
+tri_rapide([X],[X]):- !.
+tri_rapide([X|XS],L) :- balance(X,XS,L1,L2), tri_rapide(L1,L3), tri_rapide(L2,L4), concatene(L3,[X|L4],L).
+
+% Q16
+est_vide([]).
+
+% Q17
+ajoute_ensemble(X,[],[X]).
+ajoute_ensemble(X,[Y|YS],[Y|YS]):- X=Y.
+ajoute_ensemble(X,[Y|YS],[Y|L]):- ajoute_ensemble(X,YS,L).
+
+
