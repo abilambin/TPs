60ec59eca5a4df74a1ba2ae95b928fb9d53566b0
“LoicFabre” <loic.fabre@etudiant.univ-lille1.fr>
2016-02-02 13:07:17 +0100

    PF tp3 finished

--- PF/tp3/tp3.hs
+++ PF/tp3/tp3.hs
@@ -79,22 +79,105 @@ interpreteSymbole conf (etat,xs) s | s=='F' = (avance conf etat,fst(avance conf
                                    | s=='-' = (tourneADroite conf etat,xs)
                                    | otherwise = (etat,xs)
 -- 9
-aux :: Config -> Path -> Mot -> Path
-aux _ _ [] = error "Mot vide"
-aux conf points [x] = fst(fst(interpreteSymbole conf (etatInitial conf,[]) x)):points
-aux conf@(etat,l,ech,a,zs) points (x:xs) = aux ( fst(interpreteSymbole conf (etat, []) x),l,ech,a,zs) (fst(etat):points) (xs)
+aux :: Config -> Mot -> Path -> Path
+aux _ [] _ = error "Mot vide"
+aux conf [x] points = fst(fst(interpreteSymbole conf (etatInitial conf,[]) x)):points
+aux conf@(etat,l,ech,a,zs) (x:xs) points = aux ( fst(interpreteSymbole conf (etat, []) x),l,ech,a,zs) (xs) (fst(etat):points)
 
 interpreteMot :: Config -> Mot -> Picture
-interpreteMot conf xs = Line points where
-                             points = (aux conf [] xs)
+interpreteMot conf xs = Line (aux conf xs [])
 
 dessin = interpreteMot (((-150,0),0),100,1,pi/3,"F+-") "F+F--F+F"
 main = display (InWindow "L-système" (1000, 1000) (0, 0)) white dessin
 
 
+-- 10
+lsystemeAnime :: LSysteme -> Config -> Float -> Picture
+lsystemeAnime sys config t = interpreteMot newConf (sys !! iteration)
+        where iteration = round t `mod` 8
+              newConf = (etatInitial config, longueurPas config * (facteurEchelle config) ^ iteration, facteurEchelle config, angle config, symbolesTortue config)
 
---lsystemeAnime :: LSysteme -> Config -> Float -> Picture
---lsystemeAnime (x:xs) conf instant = Line (tortue conf !! enieme) where
- --                                    enieme = round instant `mod` 10
+dessin2 sys t = lsystemeAnime sys (((-150,0),0),100,1,pi/3,"F+-") t
+main2 sys t= display (InWindow "L-système" (1000, 1000) (0, 0)) white (dessin2 sys t)
 
---tortue conf  = iterate aux 
+
+vonKoch1 :: LSysteme
+vonKoch1 = lsysteme "F" regles
+    where regles 'F' = "F-F++F-F"
+          regles  s  = [s]
+
+vonKoch2 :: LSysteme
+vonKoch2 = lsysteme "F++F++F++" regles
+    where regles 'F' = "F-F++F-F"
+          regles  s  = [s]
+
+hilbert :: LSysteme
+hilbert = lsysteme "X" regles
+    where regles 'X' = "+YF-XFX-FY+"
+          regles 'Y' = "-XF+YFY+FX-"
+          regles  s  = [s]
+
+dragon :: LSysteme
+dragon = lsysteme "FX" regles
+    where regles 'X' = "X+YF+"
+          regles 'Y' = "-FX-Y"
+          regles  s  = [s]
+
+vonKoch1Anime :: Float -> Picture
+vonKoch1Anime = lsystemeAnime vonKoch1 (((-400, 0), 0), 800, 1/3, pi/3, "F+-")
+
+vonKoch2Anime :: Float -> Picture
+vonKoch2Anime = lsystemeAnime vonKoch2 (((-400, -250), 0), 800, 1/3, pi/3, "F+-")
+
+hilbertAnime :: Float -> Picture
+hilbertAnime = lsystemeAnime hilbert (((-400, -400), 0), 800, 1/2, pi/2, "F+-")
+
+dragonAnime :: Float -> Picture
+dragonAnime = lsystemeAnime dragon (((0, 0), 0), 50, 1, pi/2, "F+-")
+
+-- 11
+type EtatDessin2 = ([EtatTortue], [Path])
+
+-- 12
+-- Adaptons tout d'abord l'interpretation des symboles. 
+interpreteSymbole2 :: Config -> EtatDessin2 -> Symbole -> EtatDessin2
+interpreteSymbole2 config (etat:xs, path:ys) symb | symb == '+' = (tourneAGauche config etat:xs , path:ys)
+                                                  | symb == '-' = (tourneADroite config etat:xs , path:ys) 
+                                                  | symb == '[' = (etat:etat:xs, [fst etat]:path:ys)
+                                                  | symb == ']' = (xs, [fst (head xs)]:path:ys)
+                                                  | otherwise = (avance config etat:xs , (path ++ [fst (avance config etat)]):ys)
+interpreteSymbole2 _ _ _ = error "Not matching"
+
+
+-- Maintenant on adapte l'interpretation des mots.
+
+interpreteMots :: Config -> Mot -> Picture
+interpreteMots config mot = interpreteMots2 config (filtreSymbolesTortue config mot)
+
+interpreteMots2:: Config -> Mot -> Picture
+interpreteMots2 config mot = pictures (map line (snd (foldl (interpreteSymbole2 config) (initi, [[fst (head initi)]]) mot)) )
+        where initi = [etatInitial config]
+        
+lsystemeAnime' :: LSysteme -> Config -> Float -> Picture
+lsystemeAnime' sys config t = interpreteMots newConf (sys !! iteration)
+        where iteration = round t `mod` 6
+              newConf = (etatInitial config, longueurPas config * (facteurEchelle config) ^ iteration, facteurEchelle config, angle config, symbolesTortue config)
+
+brindille :: LSysteme
+brindille = lsysteme "F" regles
+    where regles 'F' = "F[-F]F[+F]F"
+          regles  s  = [s]
+
+broussaille :: LSysteme
+broussaille = lsysteme "F" regles
+    where regles 'F' = "FF-[-F+F+F]+[+F-F-F]"
+          regles  s  = [s]
+
+brindilleAnime :: Float -> Picture
+brindilleAnime = lsystemeAnime' brindille (((0, -400), pi/2), 800, 1/3, 25*pi/180, "F+-[]")
+
+broussailleAnime :: Float -> Picture
+broussailleAnime = lsystemeAnime' broussaille (((0, -400), pi/2), 500, 2/5, 25*pi/180, "F+-[]")
+
+mainBrindille t= display (InWindow "L-système" (1000, 1000) (0, 0)) white (brindilleAnime t)
+mainBroussaille t= display (InWindow "L-système" (1000, 1000) (0, 0)) white (broussailleAnime t)
