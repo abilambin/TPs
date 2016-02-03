7222c88405dd43bcce9b5073a5c0746362f30903
“LoicFabre” <loic.fabre@etudiant.univ-lille1.fr>
2016-02-03 15:38:02 +0100

    PF tp4

--- PF/tp4/tp4.hs
+++ PF/tp4/tp4.hs
@@ -1,7 +1,8 @@
 -- Fabre Loïc - Lambin Abigaïl - TP4
 
 module Tp4 where
-
+import Test.QuickCheck
+ 
 -- 1
 data Arbre coul val = Noeud { valeur :: val
                               , couleur :: coul
@@ -16,19 +17,49 @@ mapArbre _ Feuille  = Feuille
 mapArbre f (Noeud val coul g d) =
  Noeud (f val) coul (mapArbre f g) (mapArbre f d)
 
-foldArbre :: (a -> b -> b -> b) -> Arbre c a -> b -> b
-foldArbre _ Feuille v = v 
-foldArbre f (Noeud val _ g d) v = f val (foldArbre f g v) (foldArbre f d v)
+foldArbre :: (a -> b -> b -> b) -> b -> Arbre c a -> b
+foldArbre _ v Feuille = v 
+foldArbre f v (Noeud val _ g d) = f val (foldArbre f v g) (foldArbre f v d)
 
 -- 3
+hauteur :: Arbre c a -> Int
 hauteur Feuille  = 0
 hauteur (Noeud _ _ g d) = 1 + (max (hauteur g) (hauteur d))
 
+taille :: Arbre c a -> Int
 taille Feuille  = 0
 taille (Noeud _ _ g d) = 1 + (taille g) + (taille d)
 
 arbTest = Noeud 14 "vert" (Noeud 18 "vert" (Noeud 42 "vert" Feuille Feuille) Feuille) (Noeud 51 "vert" Feuille Feuille)
 
-hauteur'  = 
- 
+hauteur' :: Arbre c a -> Int
+hauteur' = foldArbre (\_ y z -> 1 + max y z) 0
+
+taille' :: Arbre c a -> Int
+taille' = foldArbre (\_ y z -> 1 + y + z) 0
+
 -- 4
+peigneGauche :: [(c,a)] -> Arbre c a
+peigneGauche [] = Feuille
+peigneGauche ((coul,v):xs) = Noeud v coul (peigneGauche xs) Feuille
+
+-- 5
+prop_hauteurPeigne xs = length xs == hauteur (peigneGauche xs)
+
+-- 6
+prop_taillePeigne xs = length xs == taille (peigneGauche xs)
+-- prop_map
+
+-- 7
+estComplet :: Arbre c a -> Bool
+estComplet Feuille = True
+estComplet (Noeud _ _ g d) = (taille g) == (taille d)
+
+arbCompletTest = Noeud 14 "vert" (Noeud 18 "vert" Feuille Feuille) (Noeud 51 "vert" Feuille Feuille)
+
+estComplet' :: Arbre c a -> Bool
+estComplet' Feuille = True
+estComplet' (Noeud _ _ g d) = (taille g) == (taille d)
+
+
+-- 8
