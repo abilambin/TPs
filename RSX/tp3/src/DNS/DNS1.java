ad0f5486997c759657393a4287d53eaa7e5ac6f6
LoicFabre <loic.fabre@etudiant.univ-lille1.fr>
2016-03-22 08:12:26 +0100

    RSX tp3 rendu

--- RSX/tp3/src/DNS/DNS1.java
+++
@@ -1,5 +0,0 @@
-package DNS;
-
-public class DNS1 {
-
-}
---
+++ RSX/tp3/test/test.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import UDP.*;  
+
+public class test {
+
+    // ---Pour permettre l'exécution des test----------------------
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(test.class);
+    }
+}
